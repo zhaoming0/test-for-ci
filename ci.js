58b09f9493adb2b3b7ee85fa62b150013e7f95f7
zhaoming0 <mingx.zhao@intel.com>
2019-01-22 14:37:19 +0800

    add ci.js for CI console.log process.argv

--- ci.js
+++ ci.js
@@ -1,8 +1,16 @@
 const childprocess = require('child_process');
 console.log("-----------------------------------------------------")
-childprocess.exec('git log -1 --pretty="%cE"', function (err, stdout, stderr) {
+childprocess.exec('git log -1 process.argv.slice(2) --pretty="%cE"', function (err, stdout, stderr) {
   if (err != null) {
     console.log("error is : " , err);
+  }else {
+    let data = JSON.parse(stdout);
+    console.log(data);
+    console.log("this is line 1111")
+  }
+  if (stdout){
+    console.log(stdout)
+    console.log("this is line 333333")
   }
 });
 console.log("-----------------------------------------------------")
