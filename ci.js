91ade38fb5ffb8e98259a182dcce82175bf5e1d5
zhaoming0 <mingx.zhao@intel.com>
2019-01-22 14:13:28 +0800

    add ci.js for CI console.log child-prcesss

--- ci.js
+++ ci.js
@@ -1,4 +1,9 @@
+const process = require('child_process');
 console.log("-----------------------------------------------------")
 console.log($TRAVIS_COMMIT)
 console.log("-----------------------------------------------------")
-git log -1 $TRAVIS_COMMIT --pretty="%cE"
+process.exec('git log -1 $TRAVIS_COMMIT --pretty="%cE"', function (err, stdout, stderr) {
+  if (err != null) {
+    console.log("error is : " , err);
+  }
+});
